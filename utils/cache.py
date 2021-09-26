f548eeac24b3e7ad4ce53796a11afcd1f7791f37
simsimler <sslr.mc@gmail.com>
2021-09-26 16:59:02 +0900

    feat: fix typing and add delete_user func

--- utils/cache.py
+++ utils/cache.py
@@ -10,17 +10,25 @@ class CacheClient:
         self.redis: aioredis.Redis = aioredis.from_url(
             f"redis://{self.host}:{self.port}")  # type: ignore
 
-    async def get_users(self, channel: dico.Channel) -> list[str]:
+    async def get_users(self, channel: dico.Channel.TYPING) -> list[str]:
         users: list[str] = []
 
         for i in range(0, await self.redis.llen(str(channel.id))):
-            users.append(str(await self.redis.lindex(str(channel.id), i)))
+            users.append((await self.redis.lindex(str(channel.id),
+                                                  i)).decode())
 
         return users
 
-    async def add_user(self, channel: dico.Channel,
-                       user: dico.Snowflake) -> None:
-        if str(user) in (await self.get_users(channel)):
+    async def add_user(self, channel: dico.Channel.TYPING,
+                       user: dico.User.TYPING) -> None:
+        if str(user.id) in (await self.get_users(channel)):
             return
 
         await self.redis.lpush(str(channel.id), str(user.id))
+
+    async def delete_user(self, channel: dico.Channel.TYPING,
+                          user: dico.User.TYPING) -> None:
+        if str(user.id) not in (await self.get_users(channel)):
+            return
+
+        await self.redis.lrem(str(channel.id), 1, str(user.id))
