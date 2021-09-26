ac6f1694f87bec9e76353c108fffe86ae298a6a1
simsimler <sslr.mc@gmail.com>
2021-09-26 16:59:57 +0900

    chore: drop paginator and use dp.pager

--- utils/paginator.py
+++
@@ -1,9 +0,0 @@
-import dico  # noqa
-
-
-class Paginator:
-    def __init__(self,
-                 embeds: list[dico.Embed],
-                 message: dico.Message,
-                 timeout: int = 30) -> None:
-        pass
