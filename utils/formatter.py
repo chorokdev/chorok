3e9e190cd226d63f28f4629ff3faa192ca96577a
simsimler <sslr.mc@gmail.com>
2021-10-23 01:11:55 +0900

    feat(utils.formatter): custom emoji support

--- utils/formatter.py
+++ utils/formatter.py
@@ -1,3 +1,8 @@
+PF_EMOJI = "<:position_front:901137988245946379>"
+PC_EMOJI = "<:position_current:901137988564680765>"
+PB_EMOJI = "<:position_back:901137988493410344>"
+
+
 def create_invite_link(bot_id: str, permissions: int) -> str:
     return (
         f"https://discord.com/api/oauth2/authorize"
@@ -9,7 +14,7 @@ def make_progress_bar(value: float, total: float) -> str:
     position_front = round(value / total * 16)
     position_back = 16 - position_front
 
-    return "▬" * position_front + "🔘" + "▬" * position_back
+    return PF_EMOJI * position_front + PC_EMOJI + PB_EMOJI * position_back
 
 
 def duration_format(seconds: float) -> str:
