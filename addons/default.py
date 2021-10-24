c9f0b4eeb81b910da64a23452b3b95210adbe0c1
simsimler <sslr.mc@gmail.com>
2021-10-24 18:20:46 +0900

    style(addons.music): format with yapf

--- addons/default.py
+++ addons/default.py
@@ -77,7 +77,8 @@ class Default(dico_command.Addon):  # type: ignore[call-arg, misc]
                 name=addon.name,
                 value="\n".join([
                     f"**/{command.name}{' ' + ' '.join([('<{}>' if option.required else '[{}]').format(option.name) for option in command.options])}:** `{command.description}`"
-                    for command in [inter.command for inter in addon.interactions]
+                    for command in
+                    [inter.command for inter in addon.interactions]
                     if command.description
                 ]),
                 inline=False)
